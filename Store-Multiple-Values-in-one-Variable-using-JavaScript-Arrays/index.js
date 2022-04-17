0695105f3778cb298ee21e72029604ce86b1fccd
Jonathan Zavala <jontzavala@gmail.com>
2022-04-17 13:57:01 -0500

    lesson 40 complete

--- Store-Multiple-Values-in-one-Variable-using-JavaScript-Arrays/index.js
+++ Store-Multiple-Values-in-one-Variable-using-JavaScript-Arrays/index.js
@@ -1,15 +1,14 @@
-// With JavaScript array variables, we can store several pieces of data in one place.
+// You can also nest arrays within other arrays, like below:
 
-// You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+// const teams = [["Bulls", 23], ["White Sox", 45]];
+// This is also called a multi-dimensional array.
 
-// const sandwich = ["peanut butter", "jelly", "bread"];
-// Modify the new array myArray so that it contains both a string and a number (in that order).
+// Create a nested array called myArray.
 
 
 // // Only change code below this line
 // const myArray = [];
 
 
-
 // Only change code below this line
-const myArray = ["hello", 2];
+const myArray = [["Something",2]];
